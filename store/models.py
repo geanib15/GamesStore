450a0bbf115d4418cfd52c8bdeb8b7d3c2ec011d
geanib15 <genisanaya15@gmail.com>
2019-03-14 18:11:45 +0100

    1st version with GamesList

--- store/models.py
+++ store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Game(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField(null=False)
+    genre = models.TextField()
+    description = models.TextField()
+    review = models.IntegerField()
+    price = models.FloatField()
+
+class Producer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField(null=False)
+    description = models.TextField()
+
+class Comand (models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    price = models.FloatField()
+
--- store/urls.py
+++ store/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
 from django.conf.urls import url
-from django.contrib.auth import views
+from django.views.generic import *
+from .models import *
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^store/', include (('store.urls','store'), namespace='store')),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
+   url(r'^$',
+       ListView.as_view(
+           queryset=Game.objects.all(),
+           context_object_name= 'gameslist',
+           template_name='store/gameslist.html'),
+       name='gameslist')
 ]
